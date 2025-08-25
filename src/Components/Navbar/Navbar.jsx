96b17be6f511598df241f4db909a24b29e05caca
Sridhar-1312 <ronaldosriiicr7@gmail.com>
2025-08-25 23:17:45 +0530

    editing navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,6 +4,20 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <div className='navbar'>
+      <div className="nav-logo">
+        <img src="" alt="shopping logo" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="nav-menu">
+        <li>Shop</li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src="" alt="Cart-icon" />
+      </div>
 
     </div>
   )
