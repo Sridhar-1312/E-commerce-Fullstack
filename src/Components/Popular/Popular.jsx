cf89645186f1ea3d8842430a9d605fa9832c0679
Sridhar-1312 <ronaldosriiicr7@gmail.com>
2025-08-30 23:52:40 +0530

    modified

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -4,6 +4,14 @@ import data_product from '../Assets/data'
 
 export const Popular = () => {
   return (
-    <div className='popular'>Popular</div>
-  )
+    <div className="popular">
+        <h1>POPULAR IN WOMEN</h1>
+        <hr />
+        <div className="popular-item">
+            {data_product.map((item,i) => {
+                return <Item  key={i} id ={item.id} name={item.name} image={item.image} new_price={item.new-Price} old_price={item.old_price}/>
+            })}
+        </div>
+    </div>
+)
 }
