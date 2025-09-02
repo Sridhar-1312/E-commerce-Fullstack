86da1d2c838521f95a5ae568659513ef9c4c4cea
Sridhar-1312 <ronaldosriiicr7@gmail.com>
2025-09-02 23:27:09 +0530

    added pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import  Product  from './pages/Product';
 import  Cart from './pages/Cart';
 import  LoginSignUp  from './pages/LoginSignUp';
 import Footer from './Components/footer/Footer';
-
+import men_banner from './Components/Assets/mens-banner.jpg'
+import women_banner from './Components/Assets/women-banner.jpg'
+import kid_banner from './Components/Assets/kid-banner.jpg'
 function App() {
   return (
     <div>
@@ -15,9 +17,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Shop/>} />
-        <Route path='/mens' element={<ShopCategory category="men"/>} />
-        <Route path='/womens' element={<ShopCategory category="women"/>} />
-        <Route path='/kids' element={<ShopCategory category="kid"/>} />
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>} />
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>} />
+        <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>} />
         <Route path='/product' element={<Product/>}>
           <Route path=':productId' element={<Product/>} />
         </Route>
--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,2 +1,18 @@
-import React from "react";
+import React, { createContext } from "react";
+import all_products from '../Components/Assets/all_products'
 
+
+export const ShopContext = createContext(null)
+
+const ShopContextProvider = (props)=>{
+
+       const Contextvalue = {all_products}
+
+       return (
+        <ShopContext.Provider value={Contextvalue} >
+            {props.children}
+        </ShopContext.Provider>
+       )
+}
+
+export default ShopContextProvider
