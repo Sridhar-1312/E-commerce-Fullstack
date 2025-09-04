e25cfb5d7ffdf836db0dcf4256d398aa867cec9b
Sridhar-1312 <ronaldosriiicr7@gmail.com>
2025-09-04 22:16:30 +0530

    added login page

--- src/pages/LoginSignUp.jsx
+++ src/pages/LoginSignUp.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './CSS/loginsignup.css'
 
 const LoginSignUp = () => {
   return (
-    <div>LoginSignUp</div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-feilds">
+          <input type="text" placeholder='Your Name'/>
+          <input type="email" placeholder='you Email' />
+          <input type="password" placeholder='enter password'/>
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Already Have an Acoount <span>Login here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name=''  id=''/>
+          <p>By continuing i agree to the terms of use & privacy policy</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
