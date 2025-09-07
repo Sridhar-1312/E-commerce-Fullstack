53d1d5a76e6145e8b7cf4009427850ed4e32a3e3
Sridhar-1312 <ronaldosriiicr7@gmail.com>
2025-09-07 22:47:21 +0530

    added productdisplay comp

--- src/Components/ProductsDisplay/ProductDisplay.jsx
+++ src/Components/ProductsDisplay/ProductDisplay.jsx
@@ -1,24 +1,37 @@
-import React from 'react'
-import './ProductDisplay.css'
+import React from "react";
+import "./ProductDisplay.css";
+import star_icon from "../Assets/Star Icons.png";
+import dullstar_icon from "../Assets/star-dull.png";
 
 const ProductDisplay = (props) => {
-    const {product} = props 
+  const { product } = props;
   return (
     <div className="productdisplay">
-        <div className="productdisplay-right">
-            <div className="productdisplay-img-list">
-                <img src={product.image} alt="" />
-                <img src={product.image} alt="" />
-                <img src={product.image} alt="" />
-                <img src={product.image} alt="" />
-            </div>
-            <div className="productdisplay-img">
-                <img className='productdisplay-main-img' src={product.image} alt="" />
-            </div>
+      <div className="productdisplay-left">
+        <div className="productdisplay-img-list">
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
         </div>
-        <div className="productdisplay-left"></div>
-    </div>
-  )
-}
+        <div className="productdisplay-img">
+          <img className="productdisplay-main-img" src={product.image} alt="" />
+        </div>
+      </div>
 
-export default ProductDisplay
+      <div className="productdisplay-right">
+        <h1>{product.name}</h1>
+        <div className="productdisplay-right-star">
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={dullstar_icon} alt="" />
+          <p>(122)</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDisplay;
