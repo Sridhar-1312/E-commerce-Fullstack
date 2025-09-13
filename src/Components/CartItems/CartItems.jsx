8a1fd81c274f84f661112a44d1facb0171e378a3
Sridhar-1312 <ronaldosriiicr7@gmail.com>
2025-09-13 22:32:36 +0530

    added cartitems comp

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './CartItems.css'
+import {ShopContext} from '../../context/ShopContext'
 
 const CartItems = () => {
+    const {all_product,CartItems,removeFromCart} = useContext(ShopContext);
   return (
-    <div className='CartItems'></div>
+    <div className='CartItems'>
+        <div className="cartitems-format-main">
+            <p>Products</p>
+            <p>Title</p>
+            <p>Price</p>
+            <p>Quantity</p>
+            <p>Total</p>
+            <p>Remove</p>
+        </div>
+        <hr />
+        <div>
+            <div className="cartitems-format">
+                <img src="" alt="" className='carticon-product-icon' />
+                <p></p>
+                <p></p>
+                <button className='cartitems-quantity'></button>
+                <p></p>
+                <img src=" " alt="" />
+            </div>
+        </div>
+    </div>
   )
 }
 
