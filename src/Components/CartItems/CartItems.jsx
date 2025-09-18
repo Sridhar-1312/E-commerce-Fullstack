4ec1d3df01348422d98f864a3546fb7fe68e6ea3
Sridhar-1312 <ronaldosriiicr7@gmail.com>
2025-09-18 22:57:41 +0530

    added cartitems botom

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -24,7 +24,9 @@ const CartItems = () => {
                 <img src={e.image} alt="" className="carticon-product-icon" />
                 <p>{e.name}</p>
                 <p>{e.new_price}</p>
-                <button className="cartitems-quantity">{cartItems[e.id]}</button>
+                <button className="cartitems-quantity">
+                  {cartItems[e.id]}
+                </button>
                 <p>${e.new_price * cartItems[e.id]}</p>
                 <img
                   src=""
@@ -36,7 +38,37 @@ const CartItems = () => {
             </div>
           );
         }
+        return null;
       })}
+      <div className="cartitems-down">
+        <div className="cartitems-total">
+          <h1>Cart Total</h1>
+          <div>
+            <div className="cartitems-total-item">
+              <p>Sub Total</p>
+              <p>${0}</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <p>Shipping fee</p>
+              <p>Free</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+            <h3>Total</h3>
+            <h3>${0}</h3>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cartitems-promocode">
+         <p> If you have a promocode, Enter it here</p>
+         <div className="cartitems-promobox">
+          <input type="text" placeholder="Promo Code" />
+          <button>Submit</button>
+         </div>
+        </div>
+      </div>
     </div>
   );
 };
